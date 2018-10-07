aeb2fe2553088d1b8a7783805254e0610915aa09
dimasextasy <dimaspichkalev@gmail.com>
2018-10-07 21:24:15 +0500

    create first

---
+++ venv/Include/mouse_event.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+def mouse_drawing(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Left click")
+        print(x, y)
+
+
+
+
+cap = cv2.VideoCapture("480.mp4")
+_, first_frame = cap.read()
+cv2.namedWindow("first frame")
+cv2.setMouseCallback("first frame", mouse_drawing)
+
+while True:
+    _, frame = cap.read()
+
+    #cv2.imshow("Frame", frame)
+    cv2.imshow("first frame", first_frame)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
